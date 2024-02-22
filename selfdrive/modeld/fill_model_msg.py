14d9c5550fdb2ef57e212d62daa47f3378f3e988
Yassine <yassine.y10@gmail.com>
2024-02-21 22:01:34 -0800

    more things run at 5fps here

--- selfdrive/modeld/fill_model_msg.py
+++ selfdrive/modeld/fill_model_msg.py
@@ -172,7 +172,7 @@ def fill_model_msg(msg: capnp._DynamicStructBuilder, net_output_data: dict[str, 
 
 def fill_pose_msg(msg: capnp._DynamicStructBuilder, net_output_data: dict[str, np.ndarray],
                   vipc_frame_id: int, vipc_dropped_frames: int, timestamp_eof: int, live_calib_seen: bool) -> None:
-  msg.valid = live_calib_seen & (vipc_dropped_frames < 1)
+  msg.valid = live_calib_seen & (vipc_dropped_frames < 5)
   cameraOdometry = msg.cameraOdometry
 
   cameraOdometry.frameId = vipc_frame_id
